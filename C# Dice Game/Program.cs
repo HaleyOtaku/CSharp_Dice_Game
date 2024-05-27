127932db6d1bfef9293fb381f065781fb8947d4f
HaleyOtaku <haleyotaku@gmail.com>
2024-05-27 10:10:40 -0500

    Finalized Enemy Contributions to Dice Game and Added ScoreKeeping.

--- C# Dice Game/Program.cs
+++ C# Dice Game/Program.cs
@@ -19,11 +19,65 @@ namespace C__Dice_Game
             //Incrementer(Iterator) - 
             //i++ (Same as i = i + 1)
             #endregion
+            int playerRandomNum;
+            int enemyRandomNum;
+
+            int playerPoints = 0;
+            int enemyPoints = 0;
+
+            Random random = new Random();
 
             for (int i = 0; i < 10; i++)
             {
                 Console.WriteLine("Press any key to roll the dice.");
+
+                Console.ReadKey();
+
+                //Random is lower inclusive and upper exclusive. The code below generates one number from 1 UP TO 7 but not including 7, and stores it in the integer variable playerRandomNum.
+                playerRandomNum = random.Next(1,7);
+
+                Console.WriteLine("You rolled a " + playerRandomNum);
+
+                Console.WriteLine("...");
+                //The below code makes the program wait for 1000 milliseconds (also equivalent to 1 second) before continuing on to the next line of code, since code reads top to bottom, left to right like we do.
+                System.Threading.Thread.Sleep(1000);
+
+                enemyRandomNum = random.Next(1,7);
+                Console.WriteLine("Enemy AI rolled a " + enemyRandomNum);
+
+                if (playerRandomNum > enemyRandomNum)
+                {
+                    playerPoints++;
+                    Console.WriteLine("Player wins this round!");
+                }
+                else if (playerRandomNum < enemyRandomNum)
+                {
+                    enemyPoints++;
+                    Console.WriteLine("Enemy wins this round!");
+                }
+                else
+                {
+                    Console.WriteLine("Draw!");
+                }
+
+                Console.WriteLine("The score is now - Player : " + playerPoints + ". Enemy : " + enemyPoints + ".");
+                Console.WriteLine();
             }
+
+            if (playerPoints > enemyPoints)
+            {
+                Console.WriteLine("You win!");
+            }
+            else if (playerPoints <  enemyPoints)
+            {
+                Console.WriteLine("You lose!");
+            }
+            else
+            {
+                Console.WriteLine("It's a draw!");
+            }
+
+            Console.ReadKey();
         }
     }
 }
